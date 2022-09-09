9a2f4de45dc3a840435927b52e92cb5e822c6f36
Herberth Lucho <herberth_lucho@unicomer.com>
2022-09-09 11:37:36 -0600

    fix import BrowserAnimationsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, NgbModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    NgbModule,
+    BrowserAnimationsModule,
+  ],
   providers: [],
   exports: [],
   bootstrap: [AppComponent],
